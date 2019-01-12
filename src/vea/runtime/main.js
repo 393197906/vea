ab91a842630bb957ced6f36e1f98669dad9226db
mr.xie <393197906@qq.com>
2019-01-12 10:08:44 +0800

    #add 挂载common

--- src/vea/runtime/main.js
+++ src/vea/runtime/main.js
@@ -1,4 +1,6 @@
 import vue from "vue"
 import vueRouter from "vue-router"
 import vuex,{mapActions,mapState} from "vuex"
+import common from "../extends/common.js"
+vue.use(common);
 export {vue,vueRouter,vuex,mapActions,mapState}
