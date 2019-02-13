d93fde8ddca4a74e31992e2519ec6beb37e687ee
mr.xie <393197906@qq.com>
2019-02-13 12:00:28 +0800

    #lib 编译

---
+++ build/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: ['./src/vea/runtime/main.js']
+  },
+  output: {
+    path: path.resolve(process.cwd(), './dist'),
+    publicPath: '/dist/',
+    filename: 'vea.main.js',
+    chunkFilename: '[id].js',
+    // libraryExport: 'default',
+    library: 'vea',
+    libraryTarget: 'commonjs2'
+  },
+  externals: /^(vue|react|axios|lodash).*$/i,
+  resolve: {
+    extensions: ['.js', '.json'],
+    modules: ['node_modules']
+  },
+  performance: {
+    hints: false
+  },
+  stats: {
+    children: false
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|babel|es6)$/,
+        include: process.cwd(),
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
--- src/vea/runtime/main.js
+++ src/vea/runtime/main.js
@@ -2,6 +2,5 @@ import vue from "vue"
 import vueRouter from "vue-router"
 import vuex, {mapActions, mapState} from "vuex"
 import common from "../extends/common.js"
-
 const extendVue = common
 export {vue, vueRouter, vuex, mapActions, mapState, extendVue}
